612ea6ec63afa897cb646cdc4ec3f9a7021970b0
terrinens <132042125+terrinens@users.noreply.github.com>
2024-02-06 14:02:13 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ Java/백준/Bronze V/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println( (a/b));
+            System.out.println( a%b);
+        }
+    }
+}
