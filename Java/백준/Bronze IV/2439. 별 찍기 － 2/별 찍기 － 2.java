203bd62e22a9518dfc8fed5732a2d644dab48554
terrinens <132042125+terrinens@users.noreply.github.com>
2024-02-06 14:01:34 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ Java/백준/Bronze IV/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int size = Integer.parseInt(br.readLine());
+            for (int i = 1; i < size+1; i++) {
+                wr.write(" ".repeat(size-i)  + "*".repeat(i));
+                wr.newLine();
+            }
+            wr.flush();
+        }
+    }
+}
